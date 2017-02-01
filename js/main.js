1b158bf68a6a8ab5b658e5ef5cb9ee229a4f9c5b
Jerry Vinokurov <yury.vinokurov@cfpb.gov>
2017-02-01 15:17:49 -0500

    added actr work description

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 $(function() {
-	
+
 	//$("#main").hide();
 	$("#about").hide();
 	$("#research").hide();
-	
+
 	$("#show-main").click(function() {
 		$(".content").hide();
 		$("#main").show();
 	});
-	
+
 	$("#show-about").click(function() {
 		$(".content").hide();
 		$("#about").show();
 	});
+
+	$('#act-r').click(function() {
+		$.get('html/actr.html', function(data) {
+			$('#main').html(data)
+		})
+	})
 })
